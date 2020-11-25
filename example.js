d7b575c4c20bb09f7e0e0c56e3dfeaa6cb3763fa
Akshit Goyal <goyalakshit.ag@gmail.com>
2020-11-24 21:34:27 -0500

    Added two use cases to example page

--- example.js
+++ example.js
@@ -6,6 +6,7 @@ log('Entered example.js')
 // ######################################################################
 const cards1 = new cardsGenerator('myId1')
 cards1.addCard('card9', './profilepic.jpeg', "Laura", "Pexel Model", "An awesone person who loves to go on hiking and mountain biking")
+
 // ######################################################################
 
 const cards2 = new cardsGenerator('myId2')
@@ -73,3 +74,69 @@ cards4.addLink('card1', 'http://www.google.com', 'Google')
 cards4.addLink('card1', 'http://www.facebook.com', 'Facebook')
 cards4.addLink('card1', 'http://www.linkedin.com', 'LinkedIn')
 cards4.changeThemeColorOfCard('card1',  '#F5F5DC')
+
+
+// ######################################################################
+const cards5 = new cardsGenerator('myId5')
+cards5.addCard('card1', '', "Unknown Person", "This person does not have a photo", "Woah, that means we don't have to put photo, it still works!! That's awesome.")
+
+
+// ####################################################################### USE CASES #############################
+const productLibrary = new cardsGenerator('productLibrary')
+productLibrary.addCard('firestick', './images/firestick.jpg', "Amazon Fire Stick", "Price: $34.99", "All-new Fire TV Stick with Alexa Voice Remote (includes TV controls) | HD streaming device | 2020 release.")
+productLibrary.addLink('firestick', 'https://www.amazon.ca/Staging-Product-Not-Retail-Sale/dp/B07ZZW1B86/ref=zg_bsnr_electronics_1?_encoding=UTF8&psc=1&refRID=9P83G033ED9G2VHGRRN4', 'Click here to buy')
+
+
+productLibrary.addCard('drone', './images/drone.jpg', "Drone Quadcopter", "Price: $569", "DJI Mini 2 – Ultralight and Foldable Drone Quadcopter for Adults and Kids, 3-Axis Gimbal with 4K Camera, 12MP Photo, 31 Mins Flight Time, OcuSync 2.0 10km HD Video Transmission, QuickShots, Gray")
+productLibrary.addLink('drone', 'https://www.amazon.ca/DJI-Mini-Ultralight-Quadcopter-Transmission/dp/B08JGYF5W1/ref=zg_bsnr_electronics_46?_encoding=UTF8&psc=1&refRID=9P83G033ED9G2VHGRRN4', 'Click here to buy')
+
+
+productLibrary.addCard('iphone', './images/iphone.jpg', "iPhone XS", "Price: $599.99", "Apple iPhone X, GSM Unlocked 5.8in, 64 GB - Silver (Renewed)")
+productLibrary.addLink('iphone', 'https://www.amazon.ca/Apple-iPhone-GSM-Unlocked-5-8/dp/B07757WNJP/ref=sr_1_6?dchild=1&keywords=iphone&qid=1606266678&s=electronics&sr=1-6', 'Click here to buy')
+
+productLibrary.addCard('xbox', './images/xbox.jpg', "Xbox Series S", "Price: Currently Unavailable", "Introducing the Xbox Series S, the smallest, sleekest Xbox console ever. Experience the speed and performance of a next-gen all-digital console at an accessible price point.Go all-digital and enjoy disc-free, next-gen gaming with the smallest Xbox console ever made.")
+productLibrary.addLink('xbox', 'https://www.amazon.ca/Microsoft-RRS-00001-Xbox-Series-S/dp/B08G9J44ZN?ref_=ast_sto_dp', 'Click here to buy')
+productLibrary.changeContainerBackground('#c5c5c7')
+productLibrary.changeThemeColorOfCard('firestick', '#ff9f29')
+productLibrary.changeThemeColorOfCard('drone', '#626cf5')
+productLibrary.changeThemeColorOfCard('iphone', '#db7563')
+productLibrary.changeThemeColorOfCard('xbox', '#91ed82')
+
+// ######################################################################
+
+const contacts = new cardsGenerator('contacts')
+
+contacts.addCard('Jacob', './profilepic4.jpeg', "Jacob", "Pexel Model", "An awesone person who loves to go on hiking and mountain biking")
+
+const form = document.querySelector('#addCardContact')
+form.addEventListener('submit', onFormClick)
+
+function onFormClick(e){
+    e.preventDefault(); // prevent default form action
+
+    const addName = document.querySelector('#name').value
+    console.log(addName)
+    const addTitle = document.querySelector('#title').value
+    const addBio = document.querySelector('#bio').value
+    const sMedia = document.querySelector('#smedia').value
+    const imgLink = document.querySelector('#photo').value
+    log(addTitle, addBio, sMedia, imgLink)
+        //Add card
+
+    contacts.addCard(addName, imgLink, addName, addTitle, addBio)
+    contacts.addLink(addName, addBio)
+
+
+
+}
+
+
+const delform = document.querySelector('#deleteContact')
+delform.addEventListener('submit', deleteContact)
+
+function deleteContact(e){
+    e.preventDefault(); // prevent default form action
+
+    const deleteName = document.querySelector('#dname').value
+    contacts.removeCard(deleteName)
+}
